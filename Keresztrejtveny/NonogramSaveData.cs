76e000b2545ccedf8eacbfaf74b035914d10f85d
topertz <k.markuci@gmail.com>
2026-01-29 16:56:28 +0100

    Add files via upload

---
+++ Keresztrejtveny/NonogramSaveData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonogram
+{
+    // A mentendő adatmodell
+    public class NonogramSaveData
+    {
+        public string Username { get; set; }
+        public string Difficulty { get; set; }
+        public string Mode { get; set; }
+        public int HintCount { get; set; }
+        public int WrongCellClicks { get; set; }
+        public int WrongColorClicks { get; set; }
+        public int ElapsedSeconds { get; set; }
+    }
+}
