0247cef09314f88eca5ad5dbe8a18c3ddfbc0508
topertz <k.markuci@gmail.com>
2026-03-28 18:45:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace Grafilogika_alkalmazas_keszitese
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Nonogram());
+        }
+    }
+}
